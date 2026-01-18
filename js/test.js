155e71685ee356c328be07be6d6079663ff82d1a
beatriz-araujo <beatriz@forwwward.studio>
2026-01-18 22:26:01 +0000

    test script

---
+++ js/test.js
@@ -0,0 +1,5 @@
+console.log("This is a test script - I'm living in a GitHub repo!");
+let root = document.querySelector('body');
+const newText = document.createElement('h2');
+newText.textContent = "THIS IS A TEST ENV";
+root.appendChild(newText);
