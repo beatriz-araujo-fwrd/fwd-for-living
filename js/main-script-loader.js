4fc4c4fbfbac8f9357eb89ca825e937b70427e89
beatriz-araujo <beatriz@forwwward.studio>
2026-01-19 18:52:45 +0000

    Removed DOMContentLoaded listener

--- js/main-script-loader.js
+++ js/main-script-loader.js
@@ -4,14 +4,10 @@ import { navBarMenu } from './menu.js';
 import { form } from './form.js';
 import { footerDate } from './footer-date.js';
 
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("This is coming from the main-script-loader.js");
+console.log("This is coming from the main-script-loader.js");
 
-    testScript();
-    mainInit();
-    navBarMenu();
-    form();
-    footerDate();
-
-});
-
+testScript();
+mainInit();
+navBarMenu();
+form();
+footerDate();
