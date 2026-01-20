eb106ba3aa3591b23c84911696d18394a5bbeeda
beatriz-araujo <beatriz@forwwward.studio>
2026-01-20 16:47:49 +0000

    Test update

--- js/test.js
+++ js/test.js
@@ -3,10 +3,10 @@ export function testScript() {
     // console.log("This message is coming from an uncommited .js file that is running in localhost");
     console.log("This is a test script - I'm living in a GitHub repo!");
 
-    let root = document.querySelector('body');
-    const newText = document.createElement('h2');
-    newText.textContent = "THIS IS A TEST ENV";    
-    root.appendChild(newText);
+    // let root = document.querySelector('body');
+    // const newText = document.createElement('h2');
+    // newText.textContent = "THIS IS A TEST ENV";    
+    // root.appendChild(newText);
 
     console.log("running testScript()");
 }
