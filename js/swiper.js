95fa3ecd4ba9b364781c14791e28cf07f796e2e4
beatriz-araujo <beatriz@forwwward.studio>
2026-01-28 15:53:18 +0000

    Swiper fix (desktop and mobile)
    Uniform movement in desktop (marquee effect), behavior fixes

--- js/swiper.js
+++ js/swiper.js
@@ -4,25 +4,66 @@ export function swiperInit() {
 
     // Init Homepage Works Swiper
     let hpWorksSwiper = document.querySelector('.hp_works_swiper');
-    if (hpWorksSwiper) {
+    const hpWorkSwiperSlides = document.querySelectorAll('.hp_works_slide');
 
-        hpWorksSwiper = new Swiper('.hp_works_swiper', {
-            slidesPerView: 5,
-            spaceBetween: 32,
-            // centeredSlides: true,
-            direction: 'horizontal',
-            loop: true,
-            speed: 4500, // Smooth transition speed
-            freeMode: false,
-            freeModeMomentum: false,
-            allowTouchMove: true,
-            autoWidth: true,
+    if (hpWorksSwiper && hpWorkSwiperSlides) {
 
-            autoplay: {
-                delay: 0,
-                disableOnInteraction: false,
-            },
-        });
+        setTimeout(() => {
+            hpWorksSwiper = new Swiper('.hp_works_swiper', {
+                slidesPerView: 1.8,
+                spaceBetween: 16,
+                centeredSlides: true,
+                direction: 'horizontal',
+                loop: true,
+                autoWidth: true,
+                speed: 1000,
+                autoplay: {
+                    delay: 1800,
+                    disableOnInteraction: false,
+                },
+
+                freeMode: true,
+                freeModeMomentum: false,
+                allowTouchMove: true,
+
+                breakpoints: {
+                    // for screens 768px wide and up
+                    768: {
+                        autoplay: {
+                            delay: 0,
+                            disableOnInteraction: false, // if true, will pause on hover
+                        },
+                        slidesPerView: 6,
+                        spaceBetween: 32,
+                        speed: 4000, // Smooth transition speed
+                        centeredSlides: false,
+                        freeMode: true,
+                        freeModeMomentum: false,
+                    }
+                },
+                on: {
+                    init: function () {
+                        console.log('Swiper initialized');
+
+                        // add mouse hover listener to all slides
+                        // update swiper measurements with each hover (since they expand on hover)
+                        hpWorkSwiperSlides.forEach((slide) => {
+                            slide.addEventListener('mouseenter', () => {
+                                hpWorksSwiper.update();
+                                // hpWorksSwiper.autoplay.stop();
+                            });
+                            slide.addEventListener('mouseleave', () => {
+                                hpWorksSwiper.update();
+                                // hpWorksSwiper.autoplay.start();
+                            });
+                        });
+                    },
+                }
+            });
+
+            window.addEventListener('resize', () => { hpWorksSwiper.update(); });
+
+        }, 800);
 
     }
 
