1578f5305c7670513293d454afeea07f60c94af8
beatriz-araujo <beatriz@forwwward.studio>
2026-02-17 16:56:06 +0000

    Work case href update on page load
    Fetching slug from custom field

--- js/main.js
+++ js/main.js
@@ -280,6 +280,12 @@ export function mainInit() {
                 });
             });
         }, 200);
+    // Work Case Link Redirect (for case links in Homepage and Works)
+    const workCaseLinks = document.querySelectorAll('.work_case_link');
+    if (workCaseLinks.length > 0) {
+        workCaseLinks.forEach(link => {
+            link.href += link.getAttribute('slug');
+        });
     }
 
     console.log("running mainInit()");
